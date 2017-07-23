e28f3acd6f81d59cf451e3ace3691dec15c51463
Hiep Le <arcanine1450222@gmail.com>
2017-07-23 16:04:36 +0000

    make it better

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var app = express();
 
-app.set("view engine","ejs");
-app.set("views","./views");
+app.set("view engine", "ejs");
+app.set("views", "./views");
 app.use(express.static(__dirname + '/views'))
-app.listen(525);
+app.listen(process.env.PORT || 525);
 
-app.get("/home", function (req, res) {
+app.get("/home", function(req, res) {
     res.render("home");
     console.log("Home page");
-    
+
 });
+app.get("/", function(req, res) {
+    console.log("Hello!")
+})
 console.log("Start Now!");
